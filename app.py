5f6c216d20aafb9fafc91933354d277c60b7beaf
Sanket399 <sanket.chau@gmail.com>
2025-09-30 20:48:02 +0530

    Installed Docker on Local Runner

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ def health():
 @app.route("/")
 def home():
     return "Hello from CI/CD pipeline! From VM ;)"
+    return "GitHub Actions Local Runner"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
