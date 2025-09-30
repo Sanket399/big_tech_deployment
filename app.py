f8588a291a131162e6de20811d05f44f29c6fbe8
Sanket399 <sanket.chau@gmail.com>
2025-09-30 12:52:53 +0530

    Initial commit with app.py Dockerfile and requirements.py

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/health")
+def health():
+    return "ok"
+
+@app.route("/")
+def home():
+    return "Hello from CI/CD pipeline!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
+
